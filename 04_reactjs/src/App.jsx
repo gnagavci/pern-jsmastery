40f995e771e58f3ad1d32b245290bf1d0617a1b3
Gegë Nagavci <149297494+gnagavci@users.noreply.github.com>
2026-02-07 08:53:16 +0100

    add basic frontend implementation

--- 04_reactjs/src/App.jsx
+++ 04_reactjs/src/App.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Car from "./components/Car.jsx";
 
 const App = () => {
+    const [cars, setCars] = useState([]);
+
+    useEffect(() => {
+        fetch("api/v1/cars")
+            .then((res) => res.json())
+            .then((data) => setCars(data))
+            .catch((error) => console.error("Error: ", error));
+    }, []);
+    console.log(cars);
+
     return (
         <div>
             <h1>Welcome to the Car Store</h1>
             <ul>
-                <Car />
-                <Car />
-                <Car />
+                {cars.map((car) => (
+                    <Car key={car.id} {...car} />
+                ))}
             </ul>
         </div>
     );
